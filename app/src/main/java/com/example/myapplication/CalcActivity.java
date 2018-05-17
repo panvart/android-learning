00282c8c33b181263a5dd5d3f570f20d37974b61
Pankaj <pankajkprojects@gmail.com>
2018-05-16 23:36:54 -0700

    Equation evalution done - Has Bugs

--- app/src/main/java/com/example/myapplication/CalcActivity.java
+++ app/src/main/java/com/example/myapplication/CalcActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,10 +15,129 @@ public class CalcActivity extends AppCompatActivity implements View.OnClickListe
 
     EditText etResult;
 
+    Equation equation = null;
+
+    private static final String TAG = "CALCACT_";
+
+    class Equation{
+
+        double operandLeft;
+        double operandRight;
+        char operator;
+
+        public Equation() {
+            setOperandLeftInvalid();
+            setOperandRightInvalid();
+            setOperatorInvalid();
+        }
+
+        private void setOperandLeft(String operandLeft) {
+
+            try{
+                double opLeft = Double.parseDouble(operandLeft);
+                this.operandLeft = opLeft;
+            } catch (Exception e) {
+                e.printStackTrace();
+                setOperandLeftInvalid();
+            }
+
+        }
+
+        private void setOperandRight(String operandRight) {
+
+            try{
+                double opRight = Double.parseDouble(operandRight);
+                this.operandRight = opRight;
+            } catch (Exception e) {
+                e.printStackTrace();
+                setOperandRightInvalid();
+            }
+
+        }
+
+        private void setOperandLeftInvalid(){
+            this.operandLeft = Double.NEGATIVE_INFINITY;
+        }
+
+        private void setOperandRightInvalid(){
+            this.operandRight = Double.NEGATIVE_INFINITY;
+        }
+
+        private void setOperatorInvalid(){
+            operator = Character.MIN_VALUE;
+        }
+
+        public void setOperand(String operand){
+
+            if(this.operandLeft!=Double.NEGATIVE_INFINITY)
+                setOperandRight(operand);
+            else
+                setOperandLeft(operand);
+
+        }
+
+        public void setOperator(char operator) {
+            this.operator = operator;
+        }
+
+        /**
+         *
+         * @return is equation can be evaluated
+         */
+        public boolean isEquationComplete(){
+            return this.operandLeft != Double.NEGATIVE_INFINITY
+                    && this.operandRight != Double.NEGATIVE_INFINITY
+                    && operator != Character.MIN_VALUE;
+        }
+
+        /**
+         *
+         * @return result of equation evaluation
+         */
+        public double getEvaluatedResult(){
+
+            if(!isEquationComplete())
+                return Double.MIN_VALUE;
+
+            switch (this.operator) {
+
+                case '+':
+                    return this.operandLeft + this.operandRight;
+                case '-':
+                    return this.operandLeft - this.operandRight;
+                case 'x':
+                    return this.operandLeft * this.operandRight;
+                case '/':
+                    return this.operandLeft / this.operandRight;
+                default:
+                    return Double.MIN_VALUE;
+
+            }
+
+        }
+
+        @Override
+        public String toString() {
+            return "Equation{" +
+                    "operandLeft=" + operandLeft +
+                    ", operandRight=" + operandRight +
+                    ", operator=" + operator +
+                    '}';
+        }
+
+        public void resetEquation(){
+            setOperandLeftInvalid();
+            setOperandRightInvalid();
+            setOperatorInvalid();
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
+
+        equation = new Equation();
 
         etResult = findViewById(R.id.calc_results_et);
 
@@ -93,52 +213,108 @@ public class CalcActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.calc_btn_c:
                 currResult = new StringBuilder();
+                equation.resetEquation();
                 break;
 
             case R.id.calc_btn_plus:
                 // TODO: Add code for validation of user input at run time
                 // Hint: user CurrResult last operator or use TextWatcher
                 // Color for operator using SPAN
+                equation.setOperand(currResult.toString().trim());
                 currResult.append(" + ");
+                equation.setOperator('+');
+
                 break;
 
             case R.id.calc_btn_minus:
+                equation.setOperand(currResult.toString().trim());
                 currResult.append(" - ");
+                equation.setOperator('-');
                 break;
 
             case R.id.calc_btn_divide:
+                equation.setOperand(currResult.toString().trim());
                 currResult.append(" / ");
+                equation.setOperator('/');
                 break;
 
             case R.id.calc_btn_product:
+
+                equation.setOperand(currResult.toString().trim());
                 currResult.append(" x ");
+                equation.setOperator('x');
                 break;
 
-            case R.id.calc_btn_equal:
-                //TODO: Add code fo evaluate expression
-                int result = evaluateExpression(currResult.toString());
-                currResult.append(" = ");
-                currResult.append(result);
-                break;
             default:
                 break;
 
         }
 
-        etResult.setText(currResult);
-        etResult.setSelection(currResult.length());
+        if(id==R.id.calc_btn_equal) {
+            currResult.append(" = ");
+            etResult.setText(currResult);
+            evaluateExpression();
+        } else {
+            etResult.setText(currResult);
+            etResult.setSelection(currResult.length());
+        }
+
+    }
+
+    private String getLastNumberEntered(){
+
+        String equation = etResult.getText().toString();
+
+        Log.d(TAG, "Curr String: "+equation);
+
+        if(equation.length()>0
+            && (equation.trim().endsWith("+")
+                || equation.trim().endsWith("-")
+                || equation.trim().endsWith("/")
+                || equation.trim().endsWith("x")
+                || equation.trim().endsWith("="))) {
+
+            equation = equation.trim();
+            equation = equation.substring(0, equation.length()-2);
+            equation = equation.trim();
+            equation = equation.substring(equation.lastIndexOf(' '));
+            Log.d(TAG, "After trim: "+equation);
+
+            return equation;
+
+        }
+
+        throw new IllegalArgumentException("Invalid last number");
 
     }
 
 
     /**
+     *  After evaluating INFIX expression two vars at a time
      *
-     * @param expression After evaluating INFIX expression
+     * @param
      * @return
      */
-    private int evaluateExpression(String expression){
+    private void evaluateExpression(){
 
-        return -1;
+        Log.d(TAG, "BEFORE: "+equation.toString());
+        String lastNumber = getLastNumberEntered();
+        equation.setOperandRight(lastNumber);
+        Log.d(TAG, "AFTER: "+equation.toString());
+
+        if(equation.isEquationComplete()) {
+
+            double result = equation.getEvaluatedResult();
+            etResult.setText(String.valueOf(result));
+            etResult.setSelection(etResult.getText().toString().length());
+            equation.resetEquation();
+            equation.setOperandLeft(String.valueOf(result));
+
+        } else {
+
+            etResult.append(" ERROR! ");
+
+        }
 
     }
 
